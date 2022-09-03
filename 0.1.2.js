a8a808425207f549cd73b6c979170989013a1d7b
Fkaboom <104469900+Fkaboom@users.noreply.github.com>
2022-09-02 22:38:49 -0300

    1 file modified

--- 0.1.2.js
+++ 0.1.2.js
@@ -1,17 +1,17 @@
 console.log(desconto(100, true, true));
+console.log(desconto(100, true, false));
+console.log(desconto(100, false, false));
 function desconto(compra, cartao, convenio) {
-  let desconto = 0;
   let descontoPreco = 0;
   if (cartao && convenio) {
-    descontoPreco = compra * (desconto + 0.15);
+    descontoPreco = compra * 0.15;
   } else if (cartao || convenio) {
-    descontoPreco = compra * (desconto + 0.1);
+    descontoPreco = compra * 0.1;
   }
+  console.log('----------------------------------------------------');
   console.log('Desconto:');
-
   console.log(descontoPreco);
   console.log('____________________________________________________');
   console.log('Valor com desconto:');
-
   return compra - descontoPreco;
 }
