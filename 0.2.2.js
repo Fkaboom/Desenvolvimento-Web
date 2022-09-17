f2f519826fea0c0983d21500b0e765d150cb964a
Fkaboom <104469900+Fkaboom@users.noreply.github.com>
2022-09-16 22:35:59 -0300

    1 file added

---
+++ 0.2.2.js
@@ -0,0 +1,23 @@
+const usuario = {
+  apelido: 'zé_Gameplays',
+  email: 'ZéGameplayInsane@gmail.com',
+  senha: 'ZézinGame99@',
+  dataNascimento: [15, 09, 1800],
+  localNascimento: ['São Paulo', 'São Paulo'],
+};
+
+const comentarios = {
+  usuario: ['NotZé', 'Gabriel_M@tador', 'Paulin', 'Aninha_kawai', 'Felipe0889'],
+  comentarios: [
+    'Blog incrivel, ASS: Não é o zé',
+    'Me siga de volta',
+    'Blog falido',
+    'Blog feio',
+    'Conheci antes da fama',
+    'legal',
+  ],
+};
+console.log(usuario.apelido);
+console.log(usuario.dataNascimento);
+console.log(comentarios.usuario[0]);
+console.log(comentarios.comentarios[0]);
