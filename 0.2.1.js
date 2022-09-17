4dc3378bc9e2998c7dc80938978007355bb81d03
Fkaboom <104469900+Fkaboom@users.noreply.github.com>
2022-09-16 21:09:33 -0300

    1 file added

---
+++ 0.2.1.js
@@ -0,0 +1,27 @@
+const filme1 = {
+nome: 'V de vingança',
+anoLancamento:2006,
+diretor:'James McTeigue',
+premios:[ 'Prêmio saturno melhor Atriz', 'SFX award: Melhor Atriz'],
+adaptacao:'v for vendetta',
+elenco:['Natalie Portman','Natalie Portman','Hugo Weaving','Stephen Rea','Stephenn Fry'],
+genero:['Ação','Drama','suspense','Thiller político','Ficção cientifica','Mistério']
+}
+
+const filme2 = {
+  nome: 'A viagem de Chihiro',
+  anoLancamento:2003,
+  Diretor: 'Hayao Miyazaki',
+  premios:['Oscar de Melhor Filme de Animação', 'Urso de Ouro'],
+  personagens:['Haku','Boh','Chiriro Ogino','Sem face', 'Zeniba','Fuligens'],
+  genero:['Anime','Mistério','Infantil','Amadurecimento','Fantasia','Coloring Cartoon','Aventura']
+}
+
+const filme3 = {
+  nome = 'Wall-e',
+  anoLancamento:2008,
+  diretor:'Andrew Stanton',
+  premios: 'Oscar de Melhor Filme de Animação',
+personagens:['Wall-e','Eva','Auto','Comandante MCcrea'],
+genero:['Infantil','Aventura','Ficção Cientifica','Comédia Romântica','Romance','Fantasia','Comédia','Coloring Cartoon']
+}
