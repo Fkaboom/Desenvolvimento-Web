c1b6a49cfcbdf12c113656e7b21612bde436aef9
Fkaboom <104469900+Fkaboom@users.noreply.github.com>
2022-10-07 21:53:53 -0300

    1 file added

---
+++ 0.3.1.js
@@ -0,0 +1,40 @@
+const filmes = [
+  {
+    nome: 'O Senhor do Aneis: A Sociedade do Anel',
+    lancamento: 2002,
+    diretores: ['Peter Jackson'],
+    generos: ['Fantasia', 'Aventura'],
+  },
+  {
+    nome: 'Harry Potter e a Pedra Filosofal',
+    lancamento: 2001,
+    diretores: ['Chris Columbus'],
+    generos: ['Fantasia'],
+  },
+  {
+    nome: 'Matrix',
+    lancamento: 1999,
+    diretores: ['Lana Wachowski', 'Lilly Wachowski'],
+    generos: ['Ação', 'Ficção Cientifica'],
+  },
+];
+const resultado = filmes.map((filme) => {
+  return {
+    titulo: filme.nome,
+    lancamento: filme.lancamento,
+  };
+});
+console.log(resultado);
+
+let id = 0;
+
+const contador = filmes.map((filme) => {
+  let idFilme = id;
+  id++;
+  return {
+    titulo: filme.nome,
+    lancamento: filme.lancamento,
+    idFilme,
+  };
+});
+console.log(contador);
