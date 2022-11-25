cd8e25fd0f8e57303e91d78fac1d991a56364dd9
Felipe Brandão Alanis <104469900+Fkaboom@users.noreply.github.com>
2022-11-25 19:26:38 -0300

    Create 1.8.1.js

---
+++ 1.8.1.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Personagens = (props) => {
+  const personagens = [
+    'Jill Valentine',
+		'Jack Baker',
+    'Claire Redfield',
+    'Ada Wong',
+    'Chris Redfield',
+    'Leon S. Kennedy',
+    'Albert Wesker',
+		'Lucas Baker',
+  ];
+   const personagensFiltrados = personagens.filter((personagem) => {
+  const Familia = personagem.toLowerCase().includes(props.familia);
+  return Familia;
+});
+
+  return (
+    <div>
+      
+      <ul>
+        {personagensFiltrados.map(personagem => (
+          <li>{personagem}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default function App() {
+  const [show,setShow] = React.useState('all');
+
+  const handleOnClick = (value) => setShow(value);
+
+  return (
+    <div className="App">
+      <div>
+        <h3>Mostrar Personagens de Resident Evil</h3>
+        <button onClick={()=>handleOnClick('Baker')}>Baker</button>
+        <button onClick={()=>handleOnClick('Todos')}>Todos</button>
+        <button onClick={()=>handleOnClick('Redfield')}>Redfield</button>
+        
+        {show === 'Baker' && (
+        <div>
+          <Personagens familia="baker" />
+        </div>
+      )}
+      {show === 'Redfield' && (
+        <div>
+          <Personagens familia="redfield"/>
+        </div>
+      )}
+      {show === 'Todos' && (
+        <div>
+          <Personagens familia=""/>
+        </div>
+      )}
+      </div>
+    </div>
+  );
+}
